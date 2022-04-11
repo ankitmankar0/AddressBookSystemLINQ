aa6a19a37f80de1b21aae2a32be870460ba939eb
ankitmankar0 <ankitmankar012@gmail.com>
2022-04-11 16:25:54 +0530

    Inserted Data

--- AddressBookLINQ/AddressBookTable.cs
+++ AddressBookLINQ/AddressBookTable.cs
@@ -26,6 +26,28 @@ namespace AddressBookLINQ
             table.Columns.Add("Zip", typeof(string));
             table.Columns.Add("PhoneNumber", typeof(string));
             table.Columns.Add("Email", typeof(string));
+
+            // UC3:- Ability to insert new Contacts to Address Book
+            table.Rows.Add("Ankit", "Mankar", "Kopa, Patrahi", "Nagpur", "Maharashtra", "440023", "8888866219", "ankit@gmail.com");
+            table.Rows.Add("Rohan", "Verma", "Durga kund", "Varanasi", "Uttar Pradesh", "225121", "8570934858", "ekta@gmail.com");
+            table.Rows.Add("Vishal", "Singh", "Machhalishahar", "Jaunpur", "Uttar Pradesh", "242206", "7894561230", "vishal@gmail.com.a");
         }
+
+        public void GetAllContacts()
+        {
+            foreach (DataRow dr in table.AsEnumerable())
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("FirstName:- " + dr.Field<string>("firstName"));  // you can write firstName as FirstName
+                Console.WriteLine("LastName:- " + dr.Field<string>("lastName"));
+                Console.WriteLine("Address:- " + dr.Field<string>("address"));
+                Console.WriteLine("City:- " + dr.Field<string>("city"));
+                Console.WriteLine("State:- " + dr.Field<string>("state"));
+                Console.WriteLine("Zip:- " + dr.Field<string>("zip"));
+                Console.WriteLine("PhoneNumber:- " + dr.Field<string>("phoneNumber"));
+                Console.WriteLine("Email:- " + dr.Field<string>("eMail"));
+            }
+        }
+
     }
 }
